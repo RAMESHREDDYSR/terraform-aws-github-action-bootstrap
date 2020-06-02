739d836c08736306fab161373b0376c13fa143d0
ramesh <sirigireddy.ramesh@gmail.com>
2020-06-02 11:04:00 +0530

    my changes

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = "eu-central-1"
+  region  = "us-east-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "example-terraform-project-name-bootstrap-terraform-state"
-#     key    = "default-infrastructure"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+     bucket = "ga-aws-tf"
+     key    = "default-infrastructure"
+     region = "us-east-1"
+   }
+ }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "example-terraform-project-name-bootstrap-terraform-state"
+  bucket = "ga-aws-tf"
 
   versioning {
     enabled = true
